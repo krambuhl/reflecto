1fd45adeebf1e1b7124f79c0105dc97cf9578741
Evan Krambuhl <thekrambuhl@gmail.com>
2017-12-29 17:18:59 -0800

    app outline

---
+++ packages/reflecto-app/source/components/AppContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="isMarkdown">
+      <!-- render markdown and plugin components -->
+      <Readme v-html="readme" />
+    </div>
+
+    <div v-else-if="isComponent">
+      IS COMPONENT
+    </div>
+
+    <div v-else>
+      NOT FOUND
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ packages/reflecto-app/source/components/AppShell.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="AppShell">
+    <header class="AppShell__header">
+      <ShellHeader />
+    </header>
+
+    <div class="AppShell__content" id="main">
+      <router-view></router-view>
+    </div>
+
+    <footer class="AppShell__footer">
+      <ShellFooter />
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+</style>
